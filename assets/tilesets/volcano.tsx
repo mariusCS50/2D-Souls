6bf5428cfd90ae98d4b77d17a00c7861e72dbcbb
Marius <marius.gaibu@stud.acs.upb.ro>
2024-12-09 19:40:34 +0200

    Add themed maps and corresponding tilesets
    Added the following maps:
    - Pine Forest (main lobby)
    - Winter Island
    - Lava Island
    - Crystal Cave

---
+++ assets/tilesets/volcano.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="volcano" tilewidth="32" tileheight="32" tilecount="176" columns="16">
+ <image source="../images/volcano.png" width="533" height="369"/>
+</tileset>
