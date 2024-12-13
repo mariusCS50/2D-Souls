3572b19c35625b55d839077f70d03101dabaacd4
Andrei Preda <andreipreda998@gmail.com>
2024-12-13 15:39:49 +0200

    Update melee hitbox generator

--- melee_hitbox_generator.py
+++ melee_hitbox_generator.py
@@ -2,9 +2,6 @@ import arcade
 from abc import ABC, abstractmethod
 
 class MeleeHitboxGenerator(ABC):
-    def __init__(self, owner):
-        self.owner = owner
-
     @abstractmethod
-    def generate_melee_hitbox(self, facing_dir):
+    def generate(self, owner, facing_dir):
         pass
