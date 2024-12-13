a51fbf11bbfba5c1c7f3f13374b35b721e43c441
Andrei Preda <andreipreda998@gmail.com>
2024-12-13 18:57:23 +0200

    Update health bar functionality

--- health_bar.py
+++ health_bar.py
@@ -2,10 +2,38 @@ import arcade
 import arcade.gui
 
 class HealthBar(arcade.gui.UIWidget):
-    def __init__(self, player):
+    def __init__(self, x, y, width, height, border_thickness, health, max_health):
         super().__init__()
-        self.border = arcade.gui.UISpace(x=14, y=14, height=20, width=204, color=(0, 0, 0))
-        self.bar = arcade.gui.UISpace(x=16, y=16, height=16, width=200, color=(255, 0, 0))
+        
+        percentange = health / max_health
+        
+        self.health_bar = arcade.gui.UISpace(x, y, width, height, (127, 0, 0))
+        self.fill = arcade.gui.UISpace(x, y, percentange * width, height, (255, 0, 0))
+
+        self.border = arcade.gui.UISpace(
+            x - border_thickness,
+            y - border_thickness,
+            width + 2 * border_thickness,
+            height + 2 * border_thickness,
+            (0, 0, 0))
 
         self.add(self.border)
-        self.add(self.bar)
+        self.add(self.health_bar)
+        self.add(self.fill)
+
+    def update_bar(self, health, max_health):
+        percentange = health / max_health
+        new_width = self.health_bar.width * percentange
+
+        new_fill = arcade.gui.UISpace(
+            self.fill.x,
+            self.fill.y,
+            new_width,
+            self.fill.height,
+            self.fill.color
+        )
+
+        self.remove(self.fill)
+
+        self.fill = new_fill
+        self.add(self.fill)
