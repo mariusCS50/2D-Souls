0d3bc112c0036498c35a738ee2b0ddbdb46802cb
Andrei Preda <andreipreda998@gmail.com>
2024-12-13 15:40:13 +0200

    Update MeleeWeapon

--- melee_weapon.py
+++ melee_weapon.py
@@ -2,17 +2,19 @@ import arcade
 from weapon import Weapon
 
 class MeleeWeapon(Weapon):
-    def __init__(self, damage, melee_hitbox_generator):
-        super().__init__(damage)
+    def __init__(self, owner, damage, melee_hitbox_generator):
+        super().__init__(owner, damage)
+
         self.melee_hitbox_generator = melee_hitbox_generator
         self.hitbox = None
 
     def update(self, facing_dir, scene, hit_layer_name):
         if not self.hitbox:
-            self.hitbox = self.melee_hitbox_generator.generate(facing_dir)
+            self.hitbox = self.melee_hitbox_generator.generate(self.owner, facing_dir)
 
         hit_list = arcade.check_for_collision_with_list(self.hitbox, scene[hit_layer_name])
         for hit in hit_list:
+            # TODO: Damage the hits
             pass
 
     def stop_update(self):
