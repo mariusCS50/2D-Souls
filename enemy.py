a8ee39c183ddf6238ebb0db20fb7e3c105efc28a
Andrei Preda <andreipreda998@gmail.com>
2024-12-14 18:24:52 +0200

    Made classes in game resources static
    We dont need different instances of the same class if they are all gonna have the same data

--- enemy.py
+++ enemy.py
@@ -28,13 +28,13 @@ class Enemy(arcade.Sprite, ABC):
 
         self.is_attacking = False
         self.is_idle = True
-        self.directions = EnemyResources().get_walking_directions()
+        self.directions = EnemyResources.get_walking_directions()
 
         self.collision_layers = collision_layers
         self.physics_engine = arcade.PhysicsEngineSimple(self, self.collision_layers)
 
-        self.enemy_textures = EnemyResources().get_textures(enemy_type)
-        self.weapons = WeaponResources().get_weapons()
+        self.enemy_textures = EnemyResources.get_textures(enemy_type)
+        self.weapons = WeaponResources.get_weapons()
 
         self.current_facing_direction = "down"
         self.texture = self.enemy_textures["idle"][self.current_facing_direction]
